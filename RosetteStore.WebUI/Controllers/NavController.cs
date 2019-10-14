adaf78a14c4384237bdbc5bb65a5aeb6d3c58525
Kashegor <mr.maknac@gmail.com>
2019-10-15 00:13:29 +0300

    added navig menu

---
+++ RosetteStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,28 @@
+﻿using RosetteStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RosetteStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IRosetteRepository repository;
+
+        public NavController(IRosetteRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Rosettes
+                .Select(rosette => rosette.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
