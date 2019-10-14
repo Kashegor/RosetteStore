e4b7252b71062655aa192762e92f874882f69c69
Kashegor <mr.maknac@gmail.com>
2019-10-14 22:35:29 +0300

    added bootstrup, rosette partial view. Modificated rRouteConfig

--- RosetteStore.WebUI/App_Start/RouteConfig.cs
+++ RosetteStore.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace RosetteStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Rosette", action = "List" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Rosette", action = "List", id = UrlParameter.Optional }
