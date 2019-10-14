77dbcb26ff9a8280dae9bd2c803ed7bb5d15e4ee
Kashegor <mr.maknac@gmail.com>
2019-10-14 23:30:43 +0300

    added categories, modifyed URL config

--- RosetteStore.WebUI/App_Start/RouteConfig.cs
+++ RosetteStore.WebUI/App_Start/RouteConfig.cs
@@ -13,17 +13,36 @@ namespace RosetteStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { controller = "Rosette", action = "List" }
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Rosette",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Rosette", action = "List", id = UrlParameter.Optional }
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Rosette", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
             );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Rosette", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Rosette", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
